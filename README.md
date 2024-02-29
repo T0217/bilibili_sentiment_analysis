9e519361883126f272cc7f2310488899a53a9093
ty <ty17@163.com>
2024-02-29 21:57:23 +0800

    上传主要的代码

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# bilibili_sentiment_analysis
-B站的弹幕的获取，情感分析和词云图
+# bilibili Sentiment Analysis
+
+**基于B站弹幕评论的舆情分析，B站的弹幕的获取，情感分析和词云图**
+
+## 一、代码解释
+
+代码中所参考的代码和文章：*[引用](##三、参考)*
+
+[示例代码](code/demo.py)
+
+[主函数代码](code/main.py)
+
+[自定义函数代码](code/function.py)
+
+[reruirements](requirements.txt)
+
+## 二、思路
+
+### （一）数据获取
+
+-   使用`API接口`获取弹幕数据。
+
+-   获取数据格式为`XML`格式。
+
+### （二）数据分析
+
+#### 1.视频播放中弹幕发出时间和数量
+
+1.   提取获得的`XML`文件中的节点，得到评论发出时的视频播放时长。
+
+2.   分箱，获得某一时段内的弹幕的数量。
+
+3.   可视化。
+
+4.   对弹幕数量最多的部分进行进一步分析。
+     -   视频中的内容的分析
+         -   文字解释
+     -   该段时间的弹幕内容的分析
+         -   关键词提取`（Top 10)`
+
+#### 2.弹幕实际发出的时间
+
+1.   提取获得的`XML`文件中的节点，得到评论发出时的实际时间。**（`XML`格式提取的格式为时间戳）**
+
+2.   根据不同时间节点的弹幕的数量和内容，可结合该时点的新闻或发布的政策等进行分析。
+
+#### 3.弹幕内容
+
+1.   情感分析
+     -   所有弹幕的情感倾向
+     -   情感趋势图
+2.   关键词提取
+3.   词云图
+
+#### 4.挖掘潜在信息
+
+1.   弹幕与视频内容之间的关联性。
+2.   用户在特定事件（如视频中的某个情节）发生时的弹幕行为。
+3.   用户之间的互动行为，如回复、@等。
+
+## 三、参考
+
+### （一）参考的代码
+
+-   [API接口参考](https://github.com/SocialSisterYi/bilibili-API-collect)
+-   [bilibili barrage analysis](https://github.com/moyuweiqing/bilibili-barrage-analysis)
+-   [wordCloud](https://github.com/fuqiuai/wordCloud)
+-   [popular fonts](https://github.com/chengda/popular-fonts)
+-   [stop_words](https://github.com/isnowfy/snownlp/blob/fad6ae77d6c545e09fa91b8ac90bab8864c84177/snownlp/normal/stopwords.txt)
+
+### （二）参考的方法
+
+1.   关键词提取
+     -   [TextRank](https://web.eecs.umich.edu/~mihalcea/papers/mihalcea.emnlp04.pdf)
+
---
+++ code/main.py
@@ -0,0 +1,118 @@
+# 导入类库
+import re
+import time
+import warnings
+import os
+from os.path import join, exists
+import fuction as fp
+
+# 设置库中的参数
+warnings.filterwarnings('ignore')
+
+
+# 主函数
+def main(url, segment=15, mask=False, resultpath=None):
+    '''
+    舆情分析主函数。
+
+    Parameters
+    ----------
+    url : str
+        B站的视频链接。
+        例：'https://www.bilibili.com/video/BV1wq4y1s7S5/?spm_id_from=333.337.search-card.all.click&vd_source=1d24f52164a3ed510e0b7386c010cc2e'。
+
+    segment : int, optional
+        分箱的时间间隔，默认值为15。
+
+    mask : bool, optional
+        用于指定是否采用自定义背景图片。可选值为 False 和 True。
+        默认值为 False，表示不采用自定义背景图片。
+        如果为 True，则采用自定义背景图片。
+
+    resultpath : str, optional
+        结果保存的路径。
+        例：'/home/ty/work'。
+
+    Returns
+    -------
+    None.
+
+    '''
+
+    start_time = time.time()
+
+    # 创建存储结果的路径
+    if resultpath is None:
+        resultpath = os.getcwd()
+        print('The current working directory is:', resultpath)
+        print('If you want to use the current path as your model_storage save path, please enter\'0\'.'
+              'And if you want to use another path, please enter the path.'
+              '\n', end='')
+        resultpath_input = eval(input('Please enter:'))
+        if resultpath_input == 0:
+            resultpath = resultpath
+        else:
+            resultpath = resultpath_input
+    else:
+        resultpath = resultpath
+
+    # 获取cid与存储结果的路径进行结合，如果不存在，则进行创建
+    xml_url = fp.getXML_url_(url)
+    cid = re.findall(r'com/(.*).xml', xml_url)[0]
+    resultpath = join(resultpath, cid)
+    if not exists(resultpath):
+        os.mkdir(resultpath)
+
+    # 获取可视化后的图片
+    _ = fp.getBinVisualize_(url,
+                            segment=segment,
+                            save=True,
+                            savepath=join(resultpath, 'bin_visualize.png'))
+
+    # 得到弹幕数量最多的部分的评论内容和权重最高的前十个关键词
+    topSegment_barrage, topSegment_keyTop_10 = fp.topSegmentBarrage_(url,
+                                                                     segment=segment)
+
+    topSegment_barrage.to_csv(join(resultpath, 'topSegment_barrage.csv'),
+                              index=False)
+
+    with open(join(resultpath, 'topSegment_key.txt'), 'w') as f:
+        for word in topSegment_keyTop_10:
+            f.write(f'{word}\n')
+
+    # 得到弹幕的实际发出时间和对应的弹幕
+    timeRealBarrage = fp.getBarrageTime_(xml_url,
+                                         type_='Real')
+
+    timeRealBarrage.to_csv(join(resultpath, 'timeReal_barrage.csv'),
+                           index=False)
+
+    # 情感分析
+    # 所有弹幕的情感倾向
+    sa_df = fp.sentiment_analyse_(url,
+                                  segment=segment)
+    sa_df.to_csv(join(resultpath, 'sentimentTrend.csv'),
+                 index=False)
+
+    # 情感趋势图
+    fp.sentimentTrend_(url,
+                       segment=segment,
+                       save=True,
+                       savepath=join(resultpath, 'sentimentTrend.png'))
+
+    # 全文的关键词提取
+    text_keyTop_10 = fp.topTextBarrage_(url,
+                                        segment=segment)
+    text_keyTop_10.to_csv(join(resultpath, 'topText_key.csv'),
+                          index=False)
+
+    # 词云图
+    fp.generateWordCloud_(url,
+                          segment=segment,
+                          mask=mask,
+                          savepath=join(resultpath, 'wordCloud.png'))
+
+    end_time = time.time()
+
+    print('———————————————————运行结束———————————————————')
+    print('共用时 %.2f 秒' % (end_time - start_time))
