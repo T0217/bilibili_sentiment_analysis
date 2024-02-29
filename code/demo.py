e676b3889997a599b4dba9ea81606d67c8ee7b3a
ty <ty17@163.com>
2024-02-29 23:18:09 +0800

    Committed on 2.29

--- code/demo.py
+++ code/demo.py
@@ -2,9 +2,9 @@
 from main import main
 
 # 设置B站的视频链接和结果存储路径
-url1 = 'https://www.bilibili.com/video/BV1wq4y1s7S5/?spm_id_from=333.337.search-card.all.click&vd_source=1d24f52164a3ed510e0b7386c010cc2e'
+url = 'https://www.bilibili.com/video/BV1wq4y1s7S5/?spm_id_from=333.337.search-card.all.click&vd_source=1d24f52164a3ed510e0b7386c010cc2e'
 
-resultpath = r"C:\Users\18840\Desktop"
+resultpath = './sentiment_analysis'
 
 # 运行主函数
-main(url1, segment=15, mask=False, resultpath=resultpath)
+main(url, segment=15, mask=False, resultpath=resultpath)
