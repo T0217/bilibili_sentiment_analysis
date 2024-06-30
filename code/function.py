541af3e40d4d898bf3951e3be74fe65b9ae27fde
T <tianyanggong17@gmail.com>
2024-06-30 12:46:03 +0800

    Update function.py

--- code/function.py
+++ code/function.py
@@ -79,7 +79,9 @@ def getBarrageTime_(url, type_='Video'):
     '''
 
     # 获取并解析xml文件
-    response = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleW\
+               ebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+    response = requests.get(url, headers=headers)
     response.encoding = response.apparent_encoding
     xml = response.text
 
