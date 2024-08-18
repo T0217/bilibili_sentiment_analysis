5fa657462175c0ef134978941fe26be9b512e5fb
T <tianyanggong17@gmail.com>
2024-08-18 22:24:44 +0800

    refactor: Reorganize the logic of code; fix: Multiple duplicate images appear #2

--- README.md
+++ README.md
@@ -1,24 +1,39 @@
-# bilibili Sentiment Analysis
+<h1 align="center">Bilibili Sentiment Analysis</h1>
+<p align="center"><strong>基于B站弹幕评论的舆情分析，B站的弹幕的获取，情感分析和词云图</strong></p>
 
-**基于B站弹幕评论的舆情分析，B站的弹幕的获取，情感分析和词云图**
+## 一、代码示例
 
-## 一、代码解释
+```python
+from main import main
 
-[示例代码](code/demo.py)
+# Set parameters
+url = 'https://www.bilibili.com/video/BV1wq4y1s7S5/?spm_id_from=333.337.search-card.all.click&vd_source=1d24f52164a3ed510e0b7386c010cc2e'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple\
+           WebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
+resultpath = './result'
 
-[主函数代码](code/main.py)
-
-[自定义函数代码](code/function.py)
-
-[reruirements](requirements.txt)
-
->   [!WARNING]
->
->   ***本项目仅用于学习使用，如果用于其他任何途径，本人概不负责！！！***
+# Run the main function
+main(
+    url=url,
+    headers=headers,
+    segment=15,
+    save_fig=True,
+    resultpath=resultpath
+)
+```
 
 >   [!IMPORTANT]
 >
 >   ***如果遇到有关代码的问题或是其他需求，请提 `Issues` 或 `PR` ，我会尽快解决！***
+
+>   [!WARNING]
+>
+>   ***本项目仅用于学习使用！！！***
+>
+>   ***本项目仅用于学习使用！！！***
+>
+>   ***本项目仅用于学习使用！！！***
+
 
 ## 二、思路
 
@@ -30,27 +45,24 @@
 
 ### （二）数据分析
 
-#### 1.视频播放中弹幕发出时间和数量
+#### 1. 视频播放中弹幕发出时间和数量
 
 1.   提取获得的`XML`文件中的节点，得到评论发出时的**视频播放时长**。
-
 2.   分箱，获得某一时段内的弹幕的数量。
-
 3.   可视化。
-
 4.   对弹幕数量最多的部分进行进一步分析。
      -   视频中的内容的分析
          -   文字解释
      -   该段时间的弹幕内容的分析
-         -   关键词提取`（Top 10)`
+         -   关键词提取 `(TextRank)`
 
-#### 2.弹幕实际发出的时间
+#### 2. 弹幕实际发出的时间
 
 1.   提取获得的`XML`文件中的节点，得到评论发出时的**实际时间**。**（`XML`格式提取的格式为时间戳）**
 
 2.   根据不同时间节点的弹幕的数量和内容，可结合该时点的新闻或发布的政策等进行分析。
 
-#### 3.弹幕内容
+#### 3. 弹幕内容
 
 1.   情感分析
      -   所有弹幕的情感倾向
@@ -58,7 +70,7 @@
 2.   关键词提取
 3.   词云图
 
-#### 4.挖掘潜在信息
+#### 4. 挖掘潜在信息
 
 1.   弹幕与视频内容之间的关联性。
 2.   用户在特定事件（如视频中的某个情节）发生时的弹幕行为。
--- code/main.py
+++ code/main.py
@@ -1,118 +1,149 @@
-# 导入类库
 import re
 import time
 import warnings
 import os
 from os.path import join, exists
-import function as fp
+import pandas as pd
+from typing import Dict
+from utils import (
+    set_path,
+    get_xml_url
+)
+from get_barrage import (
+    get_barrage,
+    barrage_num,
+    top_segment,
+    barrgae_keywords
+)
+from sentiment_analyse import (
+    sentiment_analyse,
+    sentiment_trend
+)
+from visualize import (
+    visualize_barrage_num,
+    visualize_sentiment_trend,
+    generate_word_cloud
+)
+from word_frequency import count_word_frequency
 
-# 设置库中的参数
+# Ignore warnings
 warnings.filterwarnings('ignore')
 
 
-# 主函数
-def main(url, segment=15, mask=False, resultpath=None):
-    '''
-    舆情分析主函数。
-
-    Parameters
-    ----------
-    url : str
-        B站的视频链接。
-        例：'https://www.bilibili.com/video/BV1wq4y1s7S5/?spm_id_from=333.337.search-card.all.click&vd_source=1d24f52164a3ed510e0b7386c010cc2e'。
-
-    segment : int, optional
-        分箱的时间间隔，默认值为15。
-
-    mask : bool, optional
-        用于指定是否采用自定义背景图片。可选值为 False 和 True。
-        默认值为 False，表示不采用自定义背景图片。
-        如果为 True，则采用自定义背景图片。
-
-    resultpath : str, optional
-        结果保存的路径。
-        例：'/home/ty/work'。
-
-    Returns
-    -------
-    None.
-
-    '''
+def main(
+        url: str,
+        headers: Dict[str, str],
+        save_fig: bool,
+        segment: int = 15,
+        maskpath: str = None,
+        mask: bool = False,
+        resultpath: str = None
+) -> None:
 
     start_time = time.time()
 
-    # 创建存储结果的路径
-    if resultpath is None:
-        resultpath = os.getcwd()
-        print('The current working directory is:', resultpath)
-        print('If you want to use the current path as your model_storage save path, please enter\'0\'.'
-              'And if you want to use another path, please enter the path.'
-              '\n', end='')
-        resultpath_input = eval(input('Please enter:'))
-        if resultpath_input == 0:
-            resultpath = resultpath
-        else:
-            resultpath = resultpath_input
-    else:
-        resultpath = resultpath
-
-    # 获取cid与存储结果的路径进行结合，如果不存在，则进行创建
-    xml_url = fp.getXML_url_(url)
+    xml_url = get_xml_url(
+        url=url,
+        headers=headers
+    )
     cid = re.findall(r'com/(.*).xml', xml_url)[0]
+    resultpath = set_path(resultpath)
     resultpath = join(resultpath, cid)
     if not exists(resultpath):
         os.mkdir(resultpath)
 
-    # 获取可视化后的图片
-    _ = fp.getBinVisualize_(url,
-                            segment=segment,
-                            save=True,
-                            savepath=join(resultpath, 'bin_visualize.png'))
+    barrage = get_barrage(
+        url=url,
+        headers=headers,
+        type_='Video'
+    )
+    df_segment = barrage_num(
+        barrage_times=barrage['time'],
+        segment=segment
+    )
+    visualize_barrage_num(
+        data=df_segment,
+        segment=segment,
+        save=save_fig,
+        savepath=join(resultpath, 'barrage_num.png')
+    )
 
-    # 得到弹幕数量最多的部分的评论内容和权重最高的前十个关键词
-    topSegment_barrage, topSegment_keyTop_10 = fp.topSegmentBarrage_(url,
-                                                                     segment=segment)
+    # Get the content of the part with the largest number of barrage
+    # And the top ten keywords with the highest weight
+    top_segment_text, top_segment_keywords = top_segment(
+        url=url,
+        headers=headers,
+        segment=segment
+    )
 
-    topSegment_barrage.to_csv(join(resultpath, 'topSegment_barrage.csv'),
-                              index=False)
+    top_segment_text.to_csv(
+        join(resultpath, 'top_segment_barrage.csv'),
+        index=False
+    )
 
-    with open(join(resultpath, 'topSegment_key.txt'), 'w') as f:
-        for word in topSegment_keyTop_10:
-            f.write(f'{word}\n')
+    with open(join(resultpath, 'top_segment_keywords.txt'), 'w') as f:
+        for keyword in top_segment_keywords:
+            f.write(f'{keyword}\n')
 
-    # 得到弹幕的实际发出时间和对应的弹幕
-    timeRealBarrage = fp.getBarrageTime_(xml_url,
-                                         type_='Real')
+    # Get the actual time of the barrage and the content of it
+    real_time_barrage = get_barrage(
+        url=url,
+        headers=headers,
+        type_='Real'
+    )
 
-    timeRealBarrage.to_csv(join(resultpath, 'timeReal_barrage.csv'),
-                           index=False)
+    real_time_barrage.to_csv(
+        join(resultpath, 'real_time_barrage.csv'),
+        index=False
+    )
 
-    # 情感分析
-    # 所有弹幕的情感倾向
-    sa_df = fp.sentiment_analyse_(url,
-                                  segment=segment)
-    sa_df.to_csv(join(resultpath, 'sentimentTrend.csv'),
-                 index=False)
+    # sentiment analyse
+    sentiment_df = sentiment_analyse(
+        url=url,
+        headers=headers
+    )
+    sentiment_df = pd.concat([barrage['content'], sentiment_df], axis=1)
+    sentiment_df.to_csv(
+        join(resultpath, 'sentiment_trend.csv'),
+        index=False
+    )
 
-    # 情感趋势图
-    fp.sentimentTrend_(url,
-                       segment=segment,
-                       save=True,
-                       savepath=join(resultpath, 'sentimentTrend.png'))
+    # sentiment trend
+    num_tag = sentiment_trend(
+        url=url,
+        headers=headers,
+        segment=segment
+    )
+    visualize_sentiment_trend(
+        data=num_tag,
+        segment=segment,
+        save=save_fig,
+        savepath=join(resultpath, 'sentiment_trend.png')
+    )
 
-    # 全文的关键词提取
-    text_keyTop_10 = fp.topTextBarrage_(url,
-                                        segment=segment)
-    text_keyTop_10.to_csv(join(resultpath, 'topText_key.csv'),
-                          index=False)
+    # Keyword extraction for full text
+    keywords_df = barrgae_keywords(
+        url=url,
+        headers=headers
+    )
+    keywords_df.to_csv(
+        join(resultpath, 'barrage_keywords.csv'),
+        index=False)
 
-    # 词云图
-    fp.generateWordCloud_(url,
-                          segment=segment,
-                          mask=mask,
-                          savepath=join(resultpath, 'wordCloud.png'))
+    # Word cloud
+    words_frequency = count_word_frequency(
+        url=url,
+        headers=headers
+    )
+    generate_word_cloud(
+        data=words_frequency,
+        mask=mask,
+        maskpath=maskpath,
+        save=save_fig,
+        savepath=join(resultpath, 'word_cloud.png')
+    )
 
     end_time = time.time()
 
-    print('———————————————————运行结束———————————————————')
-    print('共用时 %.2f 秒' % (end_time - start_time))
+    print('-' * 30 + 'Finished' + '-' * 30)
+    print('Total time used: %.2f seconds.' % (end_time - start_time))
